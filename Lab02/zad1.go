061651e82cb0c9deba60ae570056e189bd2f0b16
Jakub Woźniak <jwozniak@inf.ug.edu.pl>
2023-03-02 09:20:10 +0100

    Fixed directory

---
+++ Lab02/zad1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	zad1()
+}
+
+func zad1() {
+	var wynik int
+	wynik = 6 / 2 * (1 + 2)
+	fmt.Println("Wynik 1:", wynik)
+	var wynik2 float64
+	wynik2 = 9 - 3*3 + 1
+	fmt.Println("Wynik 2:", wynik2)
+}
