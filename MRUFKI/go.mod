42c9b226fb5fcd10073ec3034de764a2e7934112
Jakub Woźniak <jwozniak@inf.ug.edu.pl>
2023-05-04 09:59:46 +0200

    MRUFKI v2

--- MRUFKI/go.mod
+++ MRUFKI/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/goki/freetype v0.0.0-20220119013949-7a161fd3728c // indirect
 	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.4.18
 	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/srwiley/oksvg v0.0.0-20220731023508-a61f04f16b76 // indirect
@@ -26,11 +27,20 @@ require (
 	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/tevino/abool v1.2.0 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
-	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd // indirect
-	golang.org/x/mobile v0.0.0-20211207041440-4e6c2922fdee // indirect
-	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
-	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/image v0.6.0 // indirect
+	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
+)
+
+require (
+	github.com/ebitengine/purego v0.3.0 // indirect
+	github.com/hajimehoshi/file2byteslice v0.0.0-20210813153925-5340248a8f41 // indirect
+	github.com/jezek/xgb v1.1.0 // indirect
+	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
 )
--- MRUFKI/main.go
+++ MRUFKI/main.go
@@ -2,36 +2,110 @@ package main
 
 import (
 	"flag"
+	"image/color"
+	"log"
+	"math/rand"
 
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-func main() {
-
-	myApp := app.New()
-	myWindow := myApp.NewWindow("MRUFKI")
-	myCanvas := myWindow.Canvas()
-
-	myWindow.Resize(fyne.NewSize(getSize()))
-	myWindow.SetFixedSize(true)
-	myCanvas.SetOnTypedKey(func(key *fyne.KeyEvent) {
-		if key.Name == fyne.KeyEscape {
-			myWindow.Close()
-		}
-	})
-
-	myWindow.SetContent(grid)
-
-	myWindow.ShowAndRun()
+// Game implements ebiten.Game interface.
+type Game struct {
+	layers [100][100]int
 }
 
-func getSize() (float32, float32) {
-	x := flag.Int("w", 0, "x size")
-	y := flag.Int("h", 0, "y size")
-	flag.Parse()
-	fx := float32(*x)
-	fy := float32(*y)
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
+func (g *Game) Update() error {
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			if g.layers[i][j] == 1 {
+				c := rand.Intn(4)
+				if c == 0 {
+					if i > 0 {
+						g.layers[i][j] = 0
+						g.layers[i-1][j] = 1
+					}
+				} else if c == 1 {
+					if i < 99 {
+						g.layers[i][j] = 0
+						g.layers[i+1][j] = 1
+					}
+				} else if c == 2 {
+					if j > 0 {
+						g.layers[i][j] = 0
+						g.layers[i][j-1] = 1
+					}
+				} else if c == 3 {
+					if j < 99 {
+						g.layers[i][j] = 0
+						g.layers[i][j+1] = 1
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
 
-	return fx, fy
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
+func (g *Game) Draw(screen *ebiten.Image) {
+	tileXcount := len(g.layers[0])
+	tileYcount := len(g.layers)
+
+	x, _ := screen.Size()
+	tileSize := x / tileXcount
+
+	for i := 0; i < tileXcount; i++ {
+		for j := 0; j < tileYcount; j++ {
+			if g.layers[j][i] == 0 {
+				ebitenutil.DrawRect(screen, float64(i*tileSize), float64(j*tileSize), float64(tileSize), float64(tileSize), color.White)
+			} else {
+				if g.layers[j][i] == 1 {
+					ebitenutil.DrawRect(screen, float64(i*tileSize), float64(j*tileSize), float64(tileSize), float64(tileSize), color.Black)
+				}
+			}
+
+		}
+	}
+}
+
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 500, 500
+}
+
+func main() {
+	game := &Game{
+		layers: generateMap()}
+	// Specify the window size as you like. Here, a doubled size is specified.
+	getSize()
+	ebiten.SetWindowTitle("Your game's title")
+	// Call ebiten.RunGame to start your game loop.
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getSize() {
+	w := flag.Int("w", 640, "window width")
+	h := flag.Int("h", 480, "window height")
+	flag.Parse()
+
+	ebiten.SetWindowSize(*w, *h)
+}
+
+func generateMap() [100][100]int {
+	antMap := [100][100]int{}
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100; j++ {
+			if rand.Intn(100) < 5 {
+				antMap[i][j] = rand.Intn(2)
+			}
+		}
+	}
+	return antMap
 }
